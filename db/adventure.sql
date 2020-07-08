1bb9d145398dbc5082ae4e070ea749bf2a594481
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2020-07-08 19:55:03 +0200

    [MOD] Cambiar tabla de usuarios

--- db/adventure.sql
+++ db/adventure.sql
@@ -6,17 +6,17 @@ DROP TABLE IF EXISTS usuarios CASCADE;
 
 CREATE TABLE usuarios (
     id          bigserial    PRIMARY KEY,
-    nombre      varchar(15)  NOT NULL UNIQUE,
-    contrasena  varchar(255),
+    user        varchar(25)  NOT NULL UNIQUE,
     nombre      varchar(255) NOT NULL,
     apellidos   varchar(255) NOT NULL,
-    email       varchar(50)  NOT NULL UNIQUE,
+    email       varchar(255) NOT NULL UNIQUE,
+    rol         VARCHAR(30)  NOT NULL DEFAULT 'usuario',
+    created_at  timestamp(0) NOT NULL DEFAULT current_timestamp,
+    contrasena  varchar(255),
     auth_key    varchar(255),
     foto_perfil varchar(255),
     poblacion   varchar(255),
     provincia   varchar(255),
     localidad   varchar(255),
-    estado      varchar(255),
-    rol         VARCHAR(30)  NOT NULL DEFAULT 'usuario',
-    created_at  timestamp(0) NOT NULL DEFAULT current_timestamp,
+    conexion    varchar(255),
 );
