da7ee24a0c336b8109dfcac5cded8b242c2b1653
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2020-09-22 20:38:50 +0200

    [ADD] Migración con usuario admin

---
+++ migrations/m200922_181405_user_admin_insert.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200922_181405_user_admin_insert
+ */
+class m200922_181405_user_admin_insert extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('usuarios', [
+            'username' => 'adventure',
+            'nombre' => 'adventure',
+            'apellidos' => 'adventure',
+            'email' => 'adminadventure@gmail.com',
+            'rol' => 'administrador',
+            'contrasena' => Yii::$app->security->generatePasswordHash('adventure'),
+            'auth_key' => Yii::$app->security->generateRandomString(60),
+            'poblacion' => 'Sanlúcar',
+            'provincia' => 'Cádiz',
+            'pais' => 'España',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('usuarios', ['nombre' => 'adventure']);
+    }
+
+}
