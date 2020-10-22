9cd1893bd216e282d6437a7472fb827fe2f9f8a8
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2020-10-22 21:20:33 +0200

    [ADD] Mejoro formulario del registrar

--- views/usuarios/registrar.php
+++ views/usuarios/registrar.php
@@ -12,30 +12,15 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 $js = <<< EOF
-$('#email').on('input', function(){
-    $('#email').parsley().validate();
-    // window.ParsleyValidator.addValidator('checkemail', {
-    //   validateString: function(value)
-    //   {
-    //     return $.ajax({
-    //       url:'',
-    //       method:"POST",
-    //       data: data,
-    //       success:function(data)
-    //       {
-    //         return true;
-    //       }
-    //     });
-    //   }
-    // });
-
+$('#login-form').on('input', function(){
+    $('#login-form').parsley().validate();
   });
 EOF;
 
 $this->registerJs($js);
 
 ?>
-<div class="site-login">
+<div class='site-login'>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Introduzca los siguientes datos para registrarse:</p>
@@ -49,18 +34,18 @@ $this->registerJs($js);
         ],
     ]); ?>
 
-        <?= $form->field($model, 'username')->textInput() ?>
-        <?= $form->field($model, 'nombre')->textInput(['autofocus' => true]) ?>
-        <?= $form->field($model, 'apellidos')->textInput() ?>
-        <?= $form->field($model, 'contrasena')->passwordInput() ?>
-        <?= $form->field($model, 'password_repeat')->passwordInput() ?>
-        <?= $form->field($model, 'fecha_nac')->textInput() ?>
+        <?= $form->field($model, 'username')->textInput(['class' => 'form-control input-lg parsley-validated']) ?>
+        <?= $form->field($model, 'nombre')->textInput(['autofocus' => true, 'type' => 'text']) ?>
+        <?= $form->field($model, 'apellidos')->textInput(['type' => 'text']) ?>
+        <?= $form->field($model, 'contrasena')->passwordInput(['type' => 'password', 'id' => 'password1']) ?>
+        <?= $form->field($model, 'password_repeat')->passwordInput(['type' => 'password', 'data-parsley-equalto' => '#password1']) ?>
+        <?= $form->field($model, 'fecha_nac')->textInput(['placeholder' => 'YYYY/MM/D', 'data-date-format' => 'YYYY/MM/D']) ?>
         <?= $form->field($model, 'email')->textInput(['id' => 'email', 'type' => 'email', 'data-parsley-type' => 'email']) ?>
         <?= $form->field($model, 'poblacion')->textInput() ?>
         <?= $form->field($model, 'provincia')->textInput() ?>
         <?= $form->field($model, 'pais')->textInput() ?>
-        <div class="form-group">
-            <div class="offset-sm-2">
+        <div class='form-group'>
+            <div class='offset-sm-2'>
                 <?= Html::submitButton('Registrar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
