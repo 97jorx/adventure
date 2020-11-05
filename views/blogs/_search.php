4a2ca5acc5fbe9fcd7c5f0fe24cd749b3e517f74
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2020-11-05 22:17:11 +0100

    [FIX] Arreglo la url del search blogs

--- views/blogs/_search.php
+++ views/blogs/_search.php
@@ -8,12 +8,32 @@ use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $model app\models\BlogsSearch */
 /* @var $form yii\bootstrap4\ActiveForm */
-
+$url = Yii::$app->urlManager->createUrl('blogs/index', ['blogs/index', 'actual' => $actual, 'busqueda' => $busqueda]);
 ?>
+
+<!--     
+    <div class='card my-4'>
+          <h5 class='card-header'>Búsqueda</h5>
+          <div class='card-body'>
+            ?= Html::beginForm($url, 'get') ?>
+            <div class='input-group'>
+                ?= Html::textInput('busqueda', $busqueda, ['class' => 'form-control']) ?>
+                ?= Html::textInput('actual', $actual, ['class' => 'form-control', 'hidden' => true]) ?> 
+              <span class='input-group-append'>
+                ?= Html::submitButton(Icon::show('search'), ['class' => 'btn btn-primary']) ?>
+                
+              </span>
+            ?= Html::endForm();?>
+        </div>
+      </div>
+    </div>
+
+ -->
 
 
  <?php $form = ActiveForm::begin([
      'method' => 'get',
+     'action' => Url::to(['blogs/index', 'actual' => $actual]),
 ]); ?>
 
 <div class='card my-4'>
